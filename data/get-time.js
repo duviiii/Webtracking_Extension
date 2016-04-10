da180eeb1eb4aba8b0ad6e2999d84faffc8f2e4a
duviiii <ducnq301@gmail.com>
2016-04-10 13:44:45 +0300

    fix printing time

--- data/get-time.js
+++ data/get-time.js
@@ -1,6 +1,17 @@
+function addZero(x,n) {
+    while (x.toString().length < n) {
+        x = "0" + x;
+    }
+    return x;
+}
+
 function getTime(){
-  var date = new Date();
-  var str = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
-            date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "." + date.getMilliseconds();
+  var d = new Date();
+  var h = addZero(d.getHours(), 2);
+  var m = addZero(d.getMinutes(), 2);
+  var s = addZero(d.getSeconds(), 2);
+  var ms = addZero(d.getMilliseconds(), 3);
+  var str = d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " + 
+            h + ":" + m + ":" + s + "." + ms;
   return str;
 }
